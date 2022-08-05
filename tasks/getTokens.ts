0ccb63e6d98ea20482e104b517cea85424a29bd9
gpablo6 <gpablo6@outlook.com>
2022-08-05 16:08:57 +0200

    Small fix in hardhat tasks

--- tasks/getTokens.ts
+++ tasks/getTokens.ts
@@ -1,4 +1,4 @@
-import type { Contract } from "ethers";
+import { Contract } from "ethers";
 
 import { task } from "hardhat/config";
 import { getEnvVariable, getProvider, loadJsonFile } from "../utils";
@@ -18,9 +18,17 @@ task("getToken", "Show address of token from target contract")
             contract = await hre.ethers.getContractAt(contractName, 
                 fileName.contractName);
             const connectedContract = contract.connect(provider);
-            const output = await connectedContract.getToken(tokenIndex);
+            const tokenAddress = await connectedContract.getToken(tokenIndex);
+
+            const tokenContract = await hre.ethers.getContractAt( "ERC20", tokenAddress );
+
+            const tokenName = await tokenContract.name();
+            const tokenSymbol = await tokenContract.symbol();
+            const tokenDecimals = await tokenContract.decimals();
+
+
             console.log(`The token at index ${tokenIndex} from contract ${contractName} at \
-                address ${contract.address} is: ${output}\n`);
+                address ${contract.address} is:\n\tName: ${tokenName}\tSymbol: ${tokenSymbol}\tDecimals: ${tokenDecimals}\n`);
         } catch (e) {
             console.error(e);
         };
